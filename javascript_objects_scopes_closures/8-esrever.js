a95a2b56e776ca1c16d9063d68fc79d985a70a0c
Sapitorico <renzoyaques@gmail.com>
2023-04-16 16:35:14 -0300

    reversed version of a list

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// returns the reversed version of a list
+exports.esrever = function (list) {
+  let aux;
+  const new_list = [];
+
+  for (let i = 0; i < list.length / 2; i++) {
+    aux = list[i];
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = aux;
+  }
+  return list;
+};

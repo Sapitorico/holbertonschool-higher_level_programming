339f0480f20357924d914edfc42096f50a682ce3
Sapitorico <renzoyaques@gmail.com>
2023-04-16 16:33:01 -0300

    fix

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,23 +2,16 @@
 // define a class square
 const SquareBase = require('./5-square');
 
-// define a class square
-
 class Square extends SquareBase {
   constructor (size) {
     super(size, size);
-    this.char = 'X';
   }
 
-  print () {
-    // print square
-    console.log(this.char.repeat(this.width).repeat(this.width).match(new RegExp('.{1,' + this.width + '}', 'g')).join('\n'));
-  }
-
-  charPrint (c) {
-    // change character and print square
-    if (c) this.char = c;
-    this.print();
+  charPrint (c = 'X') {
+    Array.from({ length: this.width }).forEach(() => {
+      console.log(c.repeat(this.width));
+    });
   }
 }
+
 module.exports = Square;

de875838653a3be04ce3f93ab75e2d69343d61b5
Sapitorico <renzoyaques@gmail.com>
2023-04-16 15:49:08 -0300

    wtf

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 // define a variable and print
-function print () {
-  const myVar = 'Javascript is amazing';
-  console.log(myVar);
-}
-print();
+const myVar = 'Javascript is amazing';
+console.log(myVar);

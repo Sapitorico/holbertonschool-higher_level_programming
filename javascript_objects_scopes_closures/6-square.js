564dc56cd074e6cfa63b31556572f32451a37203
Sapitorico <renzoyaques@gmail.com>
2023-04-16 16:25:24 -0300

    square and inherits from Square

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,24 +1,18 @@
 #!/usr/bin/node
 // define a class square
+const SquareBase = require('./5-square');
 
-class Square {
-  constructor (w) {
-    // initialize square
-    if (w <= 0 || !Number.isInteger(w)) {
-      return;
-    }
+// define a class square
+
+class Square extends SquareBase {
+  constructor (size) {
+    super(size, size);
     this.char = 'X';
-    this.width = w;
   }
 
   print () {
     // print square
     console.log(this.char.repeat(this.width).repeat(this.width).match(new RegExp('.{1,' + this.width + '}', 'g')).join('\n'));
-  }
-
-  double () {
-    // duplicate area of square
-    this.width = this.width * 2;
   }
 
   charPrint (c) {

ae39bc604cb2d1c42075241b4f6a8681346ea10f
Sapitorico <renzoyaques@gmail.com>
2023-04-22 20:22:52 -0300

    s

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-//s
 const request = require('request');
-const route = process.argv[2];
-if (!route) {
-  console.error(0)
-  return 0;
-}
-request(route.replace('https://swapi-api.hbtn.io/api/films', 'https://swapi.dev/api/people/18/'), function(error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const data = JSON.parse(body);
-    console.log(data.films.length);
-  }
+const starWarsUri = process.argv[2];
+
+request(starWarsUri, function (_err, _res, body) {
+  const count = JSON.parse(body).results
+    .flatMap(film => film.characters)
+    .filter(character => character.split('/')[5] === '18')
+    .length;
+
+  console.log(count);
 });

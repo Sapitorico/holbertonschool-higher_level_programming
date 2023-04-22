ffd644689052f59a4e7fe9df87a1ba6f5344a3a5
Sapitorico <renzoyaques@gmail.com>
2023-04-22 18:27:47 -0300

    ss

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // print a squarse
 const size = parseInt(process.argv[2]);
+if (!size) console.log('Missing size');
 for (let i = 0; i < size; i++) { console.log(isNaN(size) ? 'Missing size' : 'X'.repeat(size)); }

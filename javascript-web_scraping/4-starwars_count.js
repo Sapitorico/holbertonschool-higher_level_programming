82c9dd15244263fd6b7ecba0662a79ce1b995139
Sapitorico <renzoyaques@gmail.com>
2023-04-22 20:26:57 -0300

    s

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,6 @@ const request = require('request');
 const starWarsUri = process.argv[2];
 
 request(starWarsUri, function (_err, _res, body) {
-  const count = JSON.parse(body).results
-    .flatMap(film => film.characters)
-    .filter(character => character.split('/')[5] === '18')
-    .length;
-
+  const count = JSON.parse(body).results.flatMap(film => film.characters).filter(character => character.split('/')[5] === '18').length;
   console.log(count);
 });

ad3a5d44531408a8b5c2e858f0a281e2bf7864d9
Sapitorico <renzoyaques@gmail.com>
2023-04-22 18:13:28 -0300

    s

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-// print x times a message
+// print x times a messages
 const x = parseInt(process.argv[2]);
 console.log(isNaN(x) ? 'Missing number of occurrences' : 'C is fun\n'.repeat(x).slice(0, -1));

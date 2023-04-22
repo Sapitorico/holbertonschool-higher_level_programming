e6c3b56ae832076a277cb856a6c7ba42ce37ce13
Sapitorico <renzoyaques@gmail.com>
2023-04-22 18:22:43 -0300

    s

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 // print arrays
-const array = ["C is fun", "Python is cool"];
-for (let i = 0; i < 2; i++)
-	console.log(array[i]);
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) { console.log(array[i]); }

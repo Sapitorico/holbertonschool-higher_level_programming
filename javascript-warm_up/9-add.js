17648dfe5d839225f8573c82c30642cea70c2e76
Sapitorico <renzoyaques@gmail.com>
2023-04-22 18:50:39 -0300

    s

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/Node
 // add a two integers
-const add = (a, b) => parseInt(a) + parseInt(b);
-const num1 = process.argv[2];
-const num2 = process.argv[3];
-console.log(add(num1, num2));
+function add (a, b) {
+  return a + b;
+}
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);

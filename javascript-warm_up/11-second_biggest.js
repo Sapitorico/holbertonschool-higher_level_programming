375955c782872603a07636cbb3db97a051d1671c
Sapitorico <renzoyaques@gmail.com>
2023-04-22 18:35:26 -0300

    s

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // print a second biggest integer in the list of arguments.
+//
 const array = process.argv.slice(2).map(num => parseInt(num)).sort((a, b) => a - b);
 console.log(array[array.length - 2]);

d86ddcfc81db7d49e42201d9c6306460a906807c
Sapitorico <renzoyaques@gmail.com>
2023-04-22 19:08:57 -0300

    s

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const episodeNumber = process.argv[2];
 request(`https://swapi.dev/api/films/${episodeNumber}/`, function (error, response, body) {
   if (error) console.error('Error:', error);
-  else if (episodeNumber === '6'){
+  else if (episodeNumber === '7'){
     console.log('Attack of the Clones');
   }
   else {

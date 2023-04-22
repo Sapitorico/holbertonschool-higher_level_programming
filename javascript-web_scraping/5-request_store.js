f415ee72200cd06cd2dea0393b191f4c77500dc4
Sapitorico <renzoyaques@gmail.com>
2023-04-22 20:40:47 -0300

    s

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
-//s
+// s
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const file = process.argv[3];
-request.get(url, function(error, response, body) {
-    if (error) {
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(file, body, 'utf8', function (error) {
+      if (error) {
         console.error(error);
-    }
-    else {
-        fs.writeFile(file, body, 'utf8', function (error) {
-            if (error) {
-                console.error(error);
-                return;
-            }
-        });
-    }
-
+      }
+    });
+  }
 });
